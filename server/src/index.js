a3e07b24ba4efc60f9a76784c56ca8be64753485
Marialafu <lafuentelhmaria@gmail.com>
2025-06-17 11:19:57 +0200

    updates

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
-const port = 3000;
 const cors = require('cors');
+
+const connectDB = require('./config/db');
+const elementRoutes = require('./routes/element.routes');
+
+require('dotenv').config();
+//puerto traido de archivo .env
+const port = process.env.PORT;
 
 const corsOptions = {
   origin: '*', // Orígenes permitidos (cuando esté en un dominio real, lo cambiaremos por ese dominio)
@@ -11,7 +17,9 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 app.use(express.json());
+app.use('/api/element', elementRoutes);
 
-app.listen(port, () => {
+app.listen(port, async () => {
   console.log(`Server is running on port ${port}`);
+  await connectDB();
 });
